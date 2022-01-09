9eb703a4ae212e8765ba955e3834c0b37c21cc61
KimKiYoung <rlarldud6482@naver.com>
2022-01-10 01:03:38 +0900

    ContentView

---
+++ SwiftUI_Server/SwiftUI_Server/Scene/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Server
+//
+//  Created by 김기영 on 2022/01/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
