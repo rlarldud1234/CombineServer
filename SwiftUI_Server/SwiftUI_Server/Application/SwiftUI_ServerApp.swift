f9dcf11586f28b3cae459c33c7315e2e9ad55182
KimKiYoung <rlarldud6482@naver.com>
2022-01-10 01:04:03 +0900

    application

---
+++ SwiftUI_Server/SwiftUI_Server/Application/SwiftUI_ServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ServerApp.swift
+//  SwiftUI_Server
+//
+//  Created by 김기영 on 2022/01/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
