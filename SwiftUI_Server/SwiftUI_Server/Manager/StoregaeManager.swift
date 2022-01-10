02b423589a2bc363f484ec6c521f43fc46d21018
KimKiYoung <rlarldud6482@naver.com>
2022-01-11 00:17:23 +0900

    StoreManager 기능 추가

--- SwiftUI_Server/SwiftUI_Server/Manager/StoregaeManager.swift
+++ SwiftUI_Server/SwiftUI_Server/Manager/StoregaeManager.swift
@@ -8,4 +8,63 @@
 import Foundation
 import Security
 
-
+class StoregateManager {
+    
+    static let shared = StoregateManager()
+    
+    private let account = "SwiftUI_Server"
+    private let service = Bundle.main.bundleIdentifier
+    
+    let keyChainQuery: NSDictionary = [
+        kSecClass: kSecClassGenericPassword,
+        kSecAttrService: Bundle.main.bundleIdentifier ?? "default",
+        kSecAttrAccount: "SwiftUI_Server"
+    ]
+    
+    func create(_ user: TokenModel) -> Bool {
+        guard let data = try? JSONEncoder().encode(user) else { return false }
+        
+        let keyChainQuery: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service!,
+            kSecAttrAccount: account,
+            kSecValueData: data
+        ]
+        
+        SecItemDelete(keyChainQuery)
+        
+        return SecItemAdd(keyChainQuery, nil) == errSecSuccess
+        
+    }
+    
+    func read() -> TokenModel? {
+        let keyChainQuery: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service!,
+            kSecAttrAccount: account,
+            kSecReturnData: kCFBooleanTrue!,
+            kSecMatchLimit: kSecMatchLimitOne
+        ]
+        
+        var dataTypeRef: CFTypeRef?
+        let status = SecItemCopyMatching(keyChainQuery, &dataTypeRef)
+        
+        if status == errSecSuccess {
+            let retrievedData = dataTypeRef as! Data
+            let value = try? JSONDecoder().decode(TokenModel.self, from: retrievedData)
+            return value
+        } else {
+            return nil
+        }
+    }
+    
+    func delete() -> Bool {
+        let keyChainQuery: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service!,
+            kSecAttrAccount: account
+        ]
+        
+        return SecItemDelete(keyChainQuery) == noErr
+    }
+}
