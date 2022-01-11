709a67d0499de68f696762fc14a5c1f79ade4b32
KimKiYoung <rlarldud6482@naver.com>
2022-01-11 18:32:30 +0900

    MoyaProvider_Extension 설정

--- SwiftUI_Server/SwiftUI_Server/Extension/MoyaProvider_Extension.swift
+++ SwiftUI_Server/SwiftUI_Server/Extension/MoyaProvider_Extension.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import Moya
+import CombineMoya
+import Combine
+
+extension MoyaProvider {
+    
+    func requestPublihser<T: Codable>(_ target: Target, _ model: T.Type) -> AnyPublisher<T, NetworkError> {
+        return self.requestPublisher(target)
+            .map(T.self)
+            .map { return $0 }
+            .mapError { NetworkError($0) }
+            .eraseToAnyPublisher()
+    }
+    
+    func requestVoidPublihser(_ target: Target) -> AnyPublisher<Void, NetworkError> {
+        return self.requestPublisher(target)
+            .map{ _ in return }
+            .mapError { NetworkError($0) }
+            .eraseToAnyPublisher()
+    }
+    
+    func requestTokenPublisher(_ target: Target) -> AnyPublisher<Void, NetworkError> {
+        return self.requestPublisher(target)
+            .map(TokenModel.self)
+            .map { if StoregateManager.shared.create($0) { return } }
+            .mapError { NetworkError($0) }
+            .eraseToAnyPublisher()
+    }
+}
