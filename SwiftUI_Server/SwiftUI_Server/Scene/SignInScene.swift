f872c1938de87c04d9b055629b0d917ab5ef9d5b
KimKiYoung <rlarldud6482@naver.com>
2022-01-15 23:21:25 +0900

    SignInScene navigation 구현

--- SwiftUI_Server/SwiftUI_Server/Scene/SignInScene.swift
+++ SwiftUI_Server/SwiftUI_Server/Scene/SignInScene.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct SignInScene: View {
     @ObservedObject private var viewModel = SignInViewModel()
+    @State var moveSignup = false
     var body: some View {
         VStack {
             TextField("id를 입력해주세요", text: $viewModel.id)
@@ -10,7 +11,21 @@ struct SignInScene: View {
                 viewModel.login()
             }, label: {
                 Text("로그인")
+                    .padding()
+                    .fullScreenCover(isPresented: $viewModel.isSuccess, content: {
+                        CommunityScene()
+                    })
             })
+            Button(action: {
+                moveSignup.toggle()
+            }, label: {
+                Text("회원가입")
+                    .padding()
+                    .fullScreenCover(isPresented: $moveSignup, content: {
+                        SignUpScene()
+                    })
+            })
+            
         }
     }
 }
