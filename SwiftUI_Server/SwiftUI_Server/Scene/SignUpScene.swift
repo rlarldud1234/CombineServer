b780f991e4b9d37a128b4c9580b30f858afe3239
KimKiYoung <rlarldud6482@naver.com>
2022-01-15 23:20:20 +0900

    SignUpScene

--- SwiftUI_Server/SwiftUI_Server/Scene/SignUpScene.swift
+++ SwiftUI_Server/SwiftUI_Server/Scene/SignUpScene.swift
@@ -1,20 +1,33 @@
 import SwiftUI
 
 struct SignUpScene: View {
-    
+    @Environment(\.presentationMode) var presentationMode
     @StateObject private var viewModel = SignUpViewModel()
     
     var body: some View {
-        VStack {
-            TextField("name을 입력해주세요.", text: $viewModel.name)
-            TextField("id를 입력해주세요.", text: $viewModel.id)
-            TextField("pw를 입력해주세요.", text: $viewModel.pw)
-            
+        ZStack(alignment: .topLeading) {
             Button(action: {
-                viewModel.signUp()
+                presentationMode.wrappedValue.dismiss()
             }, label: {
-                Text("회원가입")
+                Image(systemName: "xmark")
+                    .font(.title)
+                    .foregroundColor(.black)
+                    .padding(2)
             })
+            
+            VStack {
+                Spacer()
+                TextField("name을 입력해주세요.", text: $viewModel.name)
+                TextField("id를 입력해주세요.", text: $viewModel.id)
+                TextField("pw를 입력해주세요.", text: $viewModel.pw)
+                
+                Button(action: {
+                    viewModel.signUp()
+                }, label: {
+                    Text("회원가입")
+                })
+                Spacer()
+            }
         }
     }
 }
